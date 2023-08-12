4c2c111c93b8667c2440ab4c4ec91dd9d976f08a
Pilleteer <patcharapol.ra@ku.th>
2023-08-13 01:18:33 +0700

    change cause of stack overflow from input n>300 to input get_of=1

--- Memory_management/test_stackandheap.cpp
+++ Memory_management/test_stackandheap.cpp
@@ -1,36 +1,41 @@
 #include <iostream>
 
+
+
+
 using namespace std;
 int get_overflow(int n){
-    return get_overflow(n+1);
+   return get_overflow(n+1);
 }
 int cnt_to_zero(int n){
-    if (n == 0){
-        return 0;
-    }
-    else if(n>300){
-        get_overflow(n);
-    }
-    else{
-        cout << "Value of n: "<< n << " Address of n:" << &n << endl;;
-        return cnt_to_zero(n-1);
-    }
+   if (n == 0){
+       return 0;
+   }
+   else{
+       cout << "Value of n: "<< n << " Address of n:" << &n << endl;;
+       return cnt_to_zero(n-1);
+   }
 }
 int keeping_malloc(int n){
-    if (n == 0){
-        return 0;
-    }
-    int *p = (int*)malloc(sizeof(int));
-    *p = n;
-    cout << "Value of n: "<< *p << " Address of n:" << p << endl;
-    n=n-1;
-    return keeping_malloc(n);
+   if (n == 0){
+       return 0;
+   }
+   int *p = (int*)malloc(sizeof(int));
+   *p = n;
+   cout << "Value of n: "<< *p << " Address of n:" << p << endl;
+   n=n-1;
+   return keeping_malloc(n);
 }
 int main() {
-    int x;
-    cout << "Enter a number: ";
-    cin >> x;
-    cnt_to_zero(x);// call stack
-    keeping_malloc(x);// call heap
-    return 0;
+   int x,get_of;
+   cout << "Enter a number: ";
+   cin >> x;
+   cout << "Do you want to overflow[enter 1 to get overflow]: ";
+   cin >> get_of;
+   if(get_of==1){
+       get_overflow(get_of);
+   }
+   cnt_to_zero(x);// call stack
+   keeping_malloc(x);// call heap
+   return 0;
 }
