65d47b51ad184d7629d327d13aeb50af600dab58
Pilleteer <patcharapol.ra@ku.th>
2023-08-20 22:32:25 +0700

    add n as input

--- IOtest/io_timecompare.py
+++ IOtest/io_timecompare.py
@@ -1,9 +1,10 @@
 import time
 
 #counting
+n=int(input("Enter a number(more than 100000 to make time compare clearer):"))
 start_not_io=time.time()
 cnt=0
-for i in range(100000):
+for i in range(n):
     cnt+=1
 end_not_io=time.time()
 print("Counting time:",end_not_io-start_not_io)
@@ -11,7 +12,7 @@ print("Counting time:",end_not_io-start_not_io)
 #I/O write
 start_io=time.time()
 with open("test.txt","w") as f:
-    for i in range(100000):
+    for i in range(n):
         f.write(str(i))
 end_io=time.time()
 print("I/O write time:",end_io-start_io)
@@ -19,7 +20,7 @@ print("I/O write time:",end_io-start_io)
 #I/O read
 start_io=time.time()
 with open("test.txt","r") as f:
-    for i in range(100000):
+    for i in range(n):
         f.read()
 end_io=time.time()
 print("I/O read time:",end_io-start_io)
